5bb9c103c3e523d5202367d104be7ed7835a16ad
akira <asa.shakira340@gmail.com>
2024-12-17 13:41:22 +0900

    ui.lua - disable smooth scrolling

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -1,9 +1,29 @@
 return {
-  "folke/snacks.nvim",
-  opts = {
-    dashboard = {
-      preset = {
-        header = [[
+  {
+    "snacks.nvim",
+    opts = {
+      indent = { enabled = true },
+      input = { enabled = true },
+      notifier = { enabled = true },
+      scope = { enabled = true },
+      scroll = { enabled = false },
+      statuscolumn = { enabled = false }, -- we set this in options.lua
+      toggle = { map = LazyVim.safe_keymap_set },
+      words = { enabled = true },
+    },
+    -- stylua: ignore
+    keys = {
+      { "<leader>n", function() Snacks.notifier.show_history() end, desc = "Notification History" },
+      { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    },
+  },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
 ██████╗ ██╗  ██╗ ██████╗ 
 ╚════██╗██║  ██║██╔═████╗
  █████╔╝███████║██║██╔██║
@@ -11,6 +31,7 @@ return {
 ██████╔╝     ██║╚██████╔╝
 ╚═════╝      ╚═╝ ╚═════╝ 
     ]],
+        },
       },
     },
   },
